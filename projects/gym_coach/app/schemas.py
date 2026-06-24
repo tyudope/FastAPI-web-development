27fc3facea23c84cc2803a53a6135c1d49551cea
dopesthetic <150394396+tyudope@users.noreply.github.com>
2026-06-24 23:12:11 +0200

    feat: Pydantic Schemas for the Request/Response is done.

--- projects/gym_coach/app/schemas.py
+++ projects/gym_coach/app/schemas.py
@@ -22,7 +22,14 @@ class Sex(str, Enum):
     FEMALE = "female"
 
 
+class Location(str, Enum):
+    HOME = "home"
+    GYM = "gym"
+    OUTDOOR = "outdoor"
+    
 
+
+# Request
 class WorkoutRequest(StrictModel):
     
     name:str | None = Field(default = None, min_length=3, max_length=50, description = "Person's name.") # Optional name.
@@ -31,10 +38,32 @@ class WorkoutRequest(StrictModel):
     weight: int = Field(..., ge = 30, le = 250, description = "Person's weight in terms of the kilograms")
     sex: Sex = Field(..., description="Biological sex, used to tailor training and calorie estimates")
     workout_days: set[Weekday] = Field(..., description = "List of training days.", min_length=1)
+    preferred_location: Location = Field(default = Location.HOME, description = "User preferred working out location.")
+    experience_level:int = Field(default = 3, ge = 1, le=5, description = "User fitness experience level between 1-5 (1 = NO EXPERIENCE, 5 = ADVANCED)")
     user_context: str | None = Field(default = None, min_length=5, max_length=200, description= "User personal context of the working out purpose.")
     
 
 
+class Exercise(StrictModel):
+
+    name:str = Field(..., min_length=2, max_length=40, description="Name of the exercise.")
+    set_count:int = Field(..., ge=1, le=10, description="Number of sets that exercise performed")
+    rep_count:int = Field(..., ge=1, le=50, description= "Number of repetition for the exercise per set.")
+    rest_period:int = Field(..., ge= 10, le = 600, description = "Rest period between sets in seconds. ")
+
+
+
+class Workout(StrictModel):
+    name:str = Field(..., min_length=2, max_length=25, description = "Name of the workout. (i.e.  PUSH, PULL, UPPER etc..)")
+    exercises:list[Exercise] = Field(..., min_length= 1, max_length=12, description = "List of Exercises")
+    day:Weekday = Field(..., description="Day which Workout will be take place.")
+    target_experience_level:int = Field(..., ge=1, le= 5,
+                    description = "The expereicne level this day was designed for; must match the user's level. ")
 
 class WorkoutResponse(StrictModel):
-    pass
+    
+    workouts:list[Workout] = Field(..., min_length=1, max_length=7, description = "Workout programmes per training day.")
+    reason:str = Field(..., min_length=100, max_length=350, description = "Reason of why this type of program fits to the user's preferences." )
+    pros:str = Field(..., min_length=10, max_length=100, description = "Pros of this workout plan.")
+    cons:str = Field(..., min_length=10, max_length=100, description = "Cons of this workout plan.")
+    
