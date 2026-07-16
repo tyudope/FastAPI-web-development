3670ae5776717127ce801c2f8030670636292bb9
dopesthetic <150394396+tyudope@users.noreply.github.com>
2026-07-17 00:58:54 +0200

    fix: typo fixed seed.py

--- projects/book_tracker/app/seed.py
+++ projects/book_tracker/app/seed.py
@@ -8,7 +8,7 @@ def seed():
         books = [
             Book(name = "Dune", category = "sci-fi", total_page = 412, finished_page = 0),
             Book(name = "The Lord of the Rings: Return of the King" , 
-                 category = "fantasy", total_page = 612, finished_pages = 0),
+                 category = "fantasy", total_page = 612, finished_page = 0),
             Book(name = "Hitchiker's Guide of the Galaxy",
                  category = "sci-fi", total_page = 353, finished_page = 0),
         ]
@@ -22,7 +22,7 @@ def seed():
 if __name__ == "__main__":
     seed()
 
-    
+
 
 
 
